97537f660cd9a197930385733173a2c66c74cde1
Lukas Krickl <lukas@krickl.dev>
2023-08-24 06:36:28 +0200

    WIP: Testing handle

--- src/libyam/test/handle.c
+++ src/libyam/test/handle.c
@@ -1,9 +1,15 @@
+#include "libyam/drain.h"
 #include "libyam/expr.h"
 #include "libyam/error.h"
+#include "libyam/config.h"
 #include "libyam/test/handle.h"
 #include "libyam/test/test.h"
 #include <cmocka.h>
 #include <stdlib.h>
 #include <string.h>
 
-void test_handle(void **state) {}
+// integration tests
+void test_handle(void **state) {
+  // struct yam_config cfg = yam_config_init();
+  {}
+}
